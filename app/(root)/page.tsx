12e496b58413f8470dc7f1c5d49c710dc6f47e47
K V Naresh Karthigeyan <nareshkarthigeyan.2005@gmail.com>
2024-01-20 00:27:36 +0530

    Basic Layout setup + Auth done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
     return (
-        <main>
-            <h1>ThirdWheel</h1>
-        </main>
+        <>
+            <h1 className="head-text text-left">Home</h1>;
+        </>
     );
 }
---
+++ components/shared/Topbar.tsx
@@ -0,0 +1,4 @@
+function Topbar() {
+    return <h1>Topvbar</h1>;
+}
+export default Topbar;
