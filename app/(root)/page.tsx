31d55e9034e0f3ce292fe59d4a8da570354c1833
K V Naresh Karthigeyan <nareshkarthigeyan.2005@gmail.com>
2024-01-20 19:59:19 +0530

    fetch posts complete

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,15 @@
+import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchPosts } from "@/lib/actions/thread.action";
 import { UserButton } from "@clerk/nextjs";
+import { User, currentUser } from "@clerk/nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+    const result = await fetchPosts(1, 30);
+    console.log(result.posts);
+    const user = await currentUser();
+
+    console.log(result);
+
     return (
         <>
             <h1 className="head-text text-left">Home</h1>;
