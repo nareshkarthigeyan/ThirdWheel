cc249fc29280ecf3f2a9634c838260d1a2eb8fc2
K V Naresh Karthigeyan <nareshkarthigeyan.2005@gmail.com>
2024-01-20 20:55:49 +0530

    Name, username, content, and icons in thread structure done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
 
     return (
         <>
-            <h1 className="head-text text-left">Home</h1>;
+            <h1 className="head-text text-left">Home</h1>
             <section className="mt-9 flex flex-col gap-10">
                 {result.posts.length === 0 ? (
                     <p className="no-result">No Posts Found</p>
@@ -22,7 +22,7 @@ export default async function Home() {
                             <ThreadCard
                                 key={post._id}
                                 id={post._id}
-                                currentUserId={user?.id}
+                                currentUserId={user?.id || ""}
                                 parentId={post.parentId}
                                 content={post.text}
                                 author={post.author}
--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,3 +1,7 @@
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import Link from "next/link";
+
 interface Props {
     id: string;
     currentUserId: string;
@@ -22,7 +26,7 @@ interface Props {
     isComment?: boolean;
 }
 
-const ThreadCard = ({
+const ThreadCard = async ({
     id,
     currentUserId,
     parentId,
@@ -31,6 +35,81 @@ const ThreadCard = ({
     community,
     createdAt,
     comments,
-}: Props) => {};
+}: Props) => {
+    const username = await currentUser();
+    return (
+        <div className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
+            <div className="flex items-start justify-between">
+                <div className="flex w-full flex-1 flex-row gap-4">
+                    <div className="flex flex-col items-center">
+                        <Link
+                            href={`/profile/${author.id}`}
+                            className="relative h-11 w-11"
+                        >
+                            <Image
+                                src={author.image}
+                                alt="Profile Image"
+                                fill
+                                className="cursor-pointer rounded-full"
+                            />
+                        </Link>
+
+                        <div className="thread-card_bar" />
+                    </div>
+
+                    <div className="flex w-full flex-col">
+                        <Link href={`/profile/${author.id}`}>
+                            <span className="cursor-pointer text-base-semibold text-light-2">
+                                {author.name}
+                            </span>
+                            <span className="cursor-pointer text-base-semibold text-gray-500 mx-2">
+                                {`@${username?.username}`}
+                            </span>
+                        </Link>
+
+                        <p className="mt-2 text-small-regular text-light-2">
+                            {content}
+                        </p>
+
+                        <div className="mt-5 flex flex-col gap-3">
+                            <div className="flex gap-3.5">
+                                <Image
+                                    src="/assets/heart-gray.svg"
+                                    alt="heart"
+                                    width={24}
+                                    height={24}
+                                    className="cursor-pointer object-contain"
+                                />
+                                <Link href={`/thread/${id}`}>
+                                    <Image
+                                        src="/assets/reply.svg"
+                                        alt="heart"
+                                        width={24}
+                                        height={24}
+                                        className="cursor-pointer object-contain"
+                                    />
+                                </Link>
+                                <Image
+                                    src="/assets/repost.svg"
+                                    alt="heart"
+                                    width={24}
+                                    height={24}
+                                    className="cursor-pointer object-contain"
+                                />
+                                <Image
+                                    src="/assets/share.svg"
+                                    alt="heart"
+                                    width={24}
+                                    height={24}
+                                    className="cursor-pointer object-contain"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default ThreadCard;
