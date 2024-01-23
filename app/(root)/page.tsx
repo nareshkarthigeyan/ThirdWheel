3ab8787dfa780eb08b1169252cb40248f08e6da6
K V Naresh Karthigeyan <nareshkarthigeyan.2005@gmail.com>
2024-01-23 17:58:06 +0530

    post skeletons + implementation (maybe?)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,20 @@
+import { ThreadSkeletons } from "@/components/cards/Skeletons";
 import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchPosts } from "@/lib/actions/thread.action";
 import { UserButton } from "@clerk/nextjs";
 import { User, currentUser } from "@clerk/nextjs/server";
+import { Suspense } from "react";
+import { array } from "zod";
 
 export default async function Home() {
+    let loading = true;
     const result = await fetchPosts(1, 30);
+    loading = false;
     // console.log(result.posts);
     const user = await currentUser();
+
+    const skeletonLength = 4;
+    const skeletonArray = Array.from({ length: skeletonLength });
 
     return (
         <>
@@ -26,28 +34,34 @@ export default async function Home() {
             </h2>
 
             <section className="mt-9 flex flex-col gap-10">
-                {result.posts.length === 0 ? (
-                    <p className="no-result text-white">
-                        No Posts Found. Post something to bring it alive.
-                    </p>
-                ) : (
-                    <>
-                        {result.posts.map((post) => (
-                            <ThreadCard
-                                key={post._id}
-                                id={post._id}
-                                currentUserId={user?.id || ""}
-                                parentId={post.parentId}
-                                content={post.text}
-                                author={post.author}
-                                community={post.community}
-                                createdAt={post.createdAt}
-                                comments={post.children}
-                                username={post.username}
-                            />
-                        ))}
-                    </>
-                )}
+                <Suspense
+                    fallback={skeletonArray.map(() => (
+                        <ThreadSkeletons />
+                    ))}
+                >
+                    {result.posts.length === 0 ? (
+                        <p className="no-result text-white">
+                            No Posts Found. Post something to bring it alive.
+                        </p>
+                    ) : (
+                        <>
+                            {result.posts.map((post) => (
+                                <ThreadCard
+                                    key={post._id}
+                                    id={post._id}
+                                    currentUserId={user?.id || ""}
+                                    parentId={post.parentId}
+                                    content={post.text}
+                                    author={post.author}
+                                    community={post.community}
+                                    createdAt={post.createdAt}
+                                    comments={post.children}
+                                    username={post.username}
+                                />
+                            ))}
+                        </>
+                    )}
+                </Suspense>
             </section>
         </>
     );
---
+++ components/cards/Skeletons.tsx
@@ -0,0 +1,43 @@
+import { Skeleton } from "../ui/skeleton";
+
+export function ThreadSkeletons() {
+    return (
+        <div className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
+            <div className="flex items-start justify-between">
+                <div className="flex w-full flex-1 flex-row gap-4">
+                    <div className="flex flex-col items-center">
+                        <Skeleton className="h-12 w-12 rounded-full" />
+                    </div>
+
+                    <div className="flex w-full flex-col">
+                        <div className="flex">
+                            <div className="space-y-2">
+                                <div className="space-y-3">
+                                    <div className="flex">
+                                        <Skeleton className="h-4 w-[120px]" />
+                                        <Skeleton className="mx-2 mt-0.5 h-3 w-[100px] bg-slate-500" />
+                                    </div>
+                                    <div className="space-y-2.5">
+                                        <Skeleton className="h-3 w-[400px]" />
+                                        <Skeleton className="h-3 w-[350px]" />
+                                        <div className="flex mt-3">
+                                            <Skeleton className="mr-3 h-4 w-[18px] bg-slate-700" />
+                                            <Skeleton className="mx-3 flex-col h-4 w-[18px] bg-slate-700" />
+                                            <Skeleton className="mx-3 flex-col h-4 w-[18px] bg-slate-700" />
+                                            <Skeleton className="mx-3 flex-col h-4 w-[18px] bg-slate-700" />
+                                        </div>
+                                        <Skeleton className="mt-4 flex-col h-2.5 w-[130px] bg-slate-500" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="flex gap-3.5"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
